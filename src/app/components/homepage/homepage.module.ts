456e860a2a87be202632dfa69b6d012b1dd3e1b1
Eli Kabasele <eliezerkabas@yahoo.fr>
2018-11-06 16:37:04 +0100

    updates bootstrap

--- src/app/components/homepage/homepage.module.ts
+++ src/app/components/homepage/homepage.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage.component';
-import { FooterModule} from '../footer/footer.module';
-import { HeaderModule} from '../header/header.module';
 import { Section1Module} from '../section1/section1.module';
 import { Section2Module} from '../section2/section2.module';
 import { Section3Module} from '../section3/section3.module';
@@ -11,8 +9,6 @@ import { CarousselModule} from '../caroussel/caroussel.module';
 @NgModule({
   imports: [
     CommonModule,
-    FooterModule,
-    HeaderModule,
     Section1Module,
     Section2Module,
     Section3Module,
