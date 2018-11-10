7c30caf61337512b52039c883a999eff185f410b
Eli Kabasele <eliezerkabas@yahoo.fr>
2018-11-10 20:52:49 +0100

    implements produkte-categories

--- src/app/components/produkt/produkt-routing.module.ts
+++ src/app/components/produkt/produkt-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProduktCategoriesComponent} from './categories/produkt-categories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Kategorie',
+    component: ProduktCategoriesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
