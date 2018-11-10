3ef0340318ed8a28a6cb00404e2ef913cac46fb3
Eli Kabasele <eliezerkabas@yahoo.fr>
2018-11-10 18:32:53 +0100

    routing implemented fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DatenschutzComponent} from './components/datenschutz/datenschutz.component';
 
 const routes: Routes = [
 
