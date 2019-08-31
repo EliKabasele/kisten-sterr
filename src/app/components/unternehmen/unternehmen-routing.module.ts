eaf6caba6d67183b0f8e45feeff723df7261a497
EliKabasele <eliezerkabas@gmail.com>
2019-09-01 00:01:25 +0200

    continue development

--- src/app/components/unternehmen/unternehmen-routing.module.ts
+++ src/app/components/unternehmen/unternehmen-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {UnternehmenComponent} from './unternehmen.component';
+import { KundenserviceComponent } from './kundenservice/kundenservice.component';
+import { LieferserviceComponent } from './lieferservice/lieferservice.component';
+import { UeberunsComponent } from './ueberuns/ueberuns.component';
 
 const routes: Routes = [
   {
     path: 'unternehmen',
     component: UnternehmenComponent
+  },
+  {
+    path: 'unternehmen/kundenservice',
+    component: KundenserviceComponent
+  },
+  {
+    path: 'unternehmen/lieferservice',
+    component: LieferserviceComponent
+  },
+  {
+    path: 'unternehmen/ueberuns',
+    component: UeberunsComponent
   }
 ];
 
--- src/app/components/unternehmen/unternehmen.module.ts
+++ src/app/components/unternehmen/unternehmen.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { UnternehmenRoutingModule } from './unternehmen-routing.module';
 import { UnternehmenComponent } from './unternehmen.component';
+import { KundenserviceComponent } from './kundenservice/kundenservice.component';
+import { LieferserviceComponent } from './lieferservice/lieferservice.component';
+import { UeberunsComponent } from './ueberuns/ueberuns.component';
 
 @NgModule({
   imports: [
     CommonModule,
     UnternehmenRoutingModule
   ],
-  declarations: [UnternehmenComponent]
+  declarations: [UnternehmenComponent, KundenserviceComponent, LieferserviceComponent, UeberunsComponent]
 })
 export class UnternehmenModule { }
