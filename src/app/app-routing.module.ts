f1c141f2b14acf0fdb587b0503ad1a83fc935634
EliKabasele <eliezerkabas@gmail.com>
2019-10-01 02:58:13 +0200

    delete datenschutz-coponent-in root module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {DatenschutzComponent} from './components/datenschutz/datenschutz.component';
 
 const routes: Routes = [
 
