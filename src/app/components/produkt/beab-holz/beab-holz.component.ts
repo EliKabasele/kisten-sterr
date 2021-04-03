1909b9ece29cc3450f145fd3ef39fe78b37af7af
EliKabasele <eliezerkabas@gmail.com>
2021-04-03 15:56:09 +0200

    image-arrays

--- src/app/components/produkt/beab-holz/beab-holz.component.ts
+++ src/app/components/produkt/beab-holz/beab-holz.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeabHolzComponent implements OnInit {
 
+  image_cards = [
+    {
+      imgSrc: '../../../../assets/images/kantkolz.jpeg',
+      imgTitle: 'Kanthölzer',
+      detailLink: '/produkte/holzhandlung/kantholz'
+    },
+    {
+      imgSrc: '../../../../assets/images/bretter.jpg',
+      imgTitle: 'Bretter',
+      detailLink: '/produkte/holzhandlung/bretter'
+    },
+    {
+      imgSrc: '../../../../assets/images/gartenholz.jpg',
+      imgTitle: 'Gartenhölzer',
+      detailLink: '/produkte/holzhandlung/gartenholz'
+    },
+    {
+      imgSrc: '../../../../assets/images/blockware.jpg',
+      imgTitle: 'Blockware',
+      detailLink: '/produkte/holzhandlung/blockware'
+    },
+    {
+      imgSrc: '../../../../assets/images/holzhandlung1.jpg',
+      imgTitle: 'Holzwerkstoffe',
+      detailLink: '/produkte/holzhandlung/holzwerkstoff'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
--- src/app/components/produkt/kisten/produkt-kisten.component.ts
+++ src/app/components/produkt/kisten/produkt-kisten.component.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProduktKistenComponent implements OnInit {
 
+  image_cards = [
+    {
+      imgSrc: '../../../../assets/images/gutKiste.jpg',
+      imgTitle: 'Exportkisten',
+      detailLink: '/produkte/kisten/exportkiste'
+    },
+    {
+      imgSrc: '../../../../assets/images/longKist.jpg',
+      imgTitle: 'Transportkisten',
+      detailLink: '/produkte/kisten/transportkiste'
+    },
+    {
+      imgSrc: '../../../../assets/images/kiste3.jpg',
+      imgTitle: 'Obstkisten',
+      detailLink: '/produkte/kisten/obstkiste'
+    },
+    {
+      imgSrc: '../../../../assets/images/transportpalette.jpg',
+      imgTitle: 'Transportpaletten',
+      detailLink: '/produkte/kisten/transportpalette'
+    },
+    {
+      imgSrc: '../../../../assets/images/gutKiste.jpg',
+      imgTitle: 'Prismen',
+      detailLink: '/produkte/kisten/prisme'
+    },
+    {
+      imgSrc: '../../../../assets/images/verschlag.jpg',
+      imgTitle: 'Verschläge',
+      detailLink: '/produkte/kisten/verschlag'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
